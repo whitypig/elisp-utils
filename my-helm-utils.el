fd59aa8f796f8c43f278327e9defab467ad4818b
whitypig <whitypig@gmail.com>
2019-12-16 15:31:53 +0900

    Able to choose hsl or hsla value.

--- my-helm-utils.el
+++ my-helm-utils.el
@@ -1,5 +1,7 @@
 ;;; my-helm-utils.el
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; For helm-color.el
 (require 'helm-color)
 
 (defun helm-color-convert-rgb-to-hsl (candidate)
@@ -8,25 +10,27 @@
            for i from 1 by 2
            ;; #'color-rgb-to-hsl expectes arguments to be between
            ;; [0.0, 1.0]
-           collect (/ (string-to-number
-                       (cl-subseq rgb i (+ i 2))
-                       16)
+           collect (/ (string-to-number (cl-subseq rgb i (+ i 2))
+                                        16)
                       255.0)
            into lst
            finally return
-           (cl-loop for elt in (apply #'color-rgb-to-hsl lst)
-                    for ix from 0
-                    collect
-                    (if (zerop ix)
-                        ;; Hue
-                        (format "%.2f" (* 360 elt))
-                      ;; Saturation and lightness
-                      (format "%d%%" (truncate (* 100 elt))))
-                    into ret
-                    finally return
-                    (concat "hsl("
-                            (mapconcat #'identity ret ", ")
-                            ")"))))
+           (helm :sources (helm-build-sync-source "hsl or hsla"
+                            :candidates (helm-color-hsl-make-candidates lst))
+                 :buffer "*helm-color-hsl*")))
+
+(defun helm-color-hsl-make-candidates (hsl-values)
+  (cl-loop for elt in (apply #'color-rgb-to-hsl hsl-values) and ix from 0
+           collect (if (zerop ix)
+                       ;; Hue
+                       (format "%.2f" (* 360 elt))
+                     ;; Saturation and lightness
+                     (format "%d%%" (truncate (* 100 elt))))
+           into values
+           finally return
+           (let ((s (mapconcat #'identity values ", ")))
+             (list (format "hsl(%s)" s)
+                   (format "hsla(%s, 1.0)" s)))))
 
 (defun helm-color-insert-hsl (candidate)
   (with-helm-current-buffer
@@ -38,7 +42,9 @@
 (defun helm-color-run-insert-hsl ()
   "Insert HSL of color from `helm-source-colors'"
   (interactive)
-  (with-helm-alive-p (helm-exit-and-execute-action 'helm-color-insert-hsl)))
+  (with-helm-alive-p
+    (helm-exit-and-execute-action 'helm-color-insert-hsl)))
+
 (put 'helm-color-run-insert-hsl 'helm-only t)
 
 (defun helm-color-run-kill-hsl ()
